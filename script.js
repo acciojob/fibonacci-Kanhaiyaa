91292aa867445bddf721d024195310eb65f18378
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-03 22:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	let n1=0,n2=1,nextterm
+
+	for (let i = 0; i <=num; i++) {
+		console.log(n1)
+		nextterm=n1+n2;
+		n1=n2;
+		n2=nextterm
+	}
 }
 
 module.exports = fibonacci;
